99f1347cd9a78387ebb856616729263241270c20
billiejoenahema <billiejoenahema@gmail.com>
2022-10-30 21:36:06 +0900

    install: fontawesome

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -6,7 +6,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).mount("#app");
+library.add(fas);
+
+createApp(App)
+  .use(router)
+  .use(pinia)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
