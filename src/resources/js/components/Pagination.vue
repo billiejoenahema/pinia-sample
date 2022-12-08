32a91502e83c84c09e08e82a2b56de4bc2589b11
billiejoenahema <billiejoenahema@gmail.com>
2022-12-08 20:08:52 +0900

    improve: pagination

--- src/resources/js/components/Pagination.vue
+++ src/resources/js/components/Pagination.vue
@@ -8,29 +8,42 @@ const props = defineProps({
 });
 const emit = defineEmits(["change"]);
 const changePage = (url = null) => {
-  const page = url?.substring(url.length - 1) || null;
+  if (!url) return;
+  const page = url.substring(url.indexOf("=") + 1);
   emit("change", page);
 };
 const linkLabel = (label) => {
-  return label.replace("&laquo;", "<<").replace("&raquo;", ">>");
+  return label.replace("&laquo; 前", "<<").replace("次 &raquo;", ">>");
 };
 </script>
 
 <template>
-  <ul class="pagination">
-    <li
-      v-for="link in links"
-      class="page-item"
-      :class="{
-        disabled: link.url === null,
-        active: link.active,
-      }"
-      :tabindex="link.url === null ? '-1' : ''"
-      @click.prevent="changePage(link.url)"
-    >
-      <a href="#" class="page-link">
-        {{ linkLabel(link.label) }}
-      </a>
-    </li>
-  </ul>
+  <nav class="pagination-nav" aria-label="...">
+    <ul class="pagination">
+      <li
+        v-for="link in links"
+        class="page-item"
+        :class="{
+          disabled: link.url === null,
+          active: link.active,
+        }"
+        :tabindex="link.url === null ? '-1' : ''"
+        @click.prevent="changePage(link.url)"
+      >
+        <a href="#" class="page-link">
+          <code>{{ linkLabel(link.label) }}</code>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
+
+<style>
+.pagination-nav {
+  display: flex;
+  justify-content: center;
+}
+.page-link {
+  user-select: none;
+}
+</style>
